a67b6862ecefd073a5bf8825c071ad7e1b3ae927
SomyaDhal <34393219+SomyaDhal@users.noreply.github.com>
2017-12-10 12:27:43 +0530

    Compare

--- src/scripts/LoginThroughInvaliddata.java
+++ src/scripts/LoginThroughInvaliddata.java
@@ -13,7 +13,7 @@ public class LoginThroughInvaliddata extends BaseTest{
 	public void testInvalidlogin() throws InterruptedException{
 	LoginPageobjectRepository plr=new LoginPageobjectRepository(driver);
 	
-	int rowCount=Lib.getRowCount(EXCEL_PATH, "Invalid Login");
+	int rowCount=Lib.getRowCount(EXCEL_PATH, "Invalid Login");//comment
 	System.out.println(rowCount);
 	
 	for (int i = 1; i <= rowCount; i++) {
